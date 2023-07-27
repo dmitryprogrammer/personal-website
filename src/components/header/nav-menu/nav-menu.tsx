6047cc0b2b7f6c59730efe7461821f9417a0ee5f
dmitryprogrammer <dpischalka@gmail.com>
2023-07-27 19:06:35 +0400

    [basics] tried styling "nav-menu" component.

--- src/components/header/nav-menu/nav-menu.tsx
+++ src/components/header/nav-menu/nav-menu.tsx
@@ -1,11 +1,13 @@
+import './nav-menu.scss';
+
 export const NavMenu = () => {
   return (
-    <nav>
-      <ul>
-        <li>Main</li>
-        <li>Blog</li>
-        <li>Books I've Read</li>
-        <li>Contacts</li>
+    <nav className="navigation-menu">
+      <ul className="navigation-menu__list">
+        <li className="navigation-menu__item">Main</li>
+        <li className="navigation-menu__item">Blog</li>
+        <li className="navigation-menu__item">Books I've Read</li>
+        <li className="navigation-menu__item">Contacts</li>
       </ul>
     </nav>
   );
