c6272b5194c0b98e6d16feeb3310172132a80096
dmitryprogrammer <dpischalka@gmail.com>
2023-07-29 14:04:58 +0400

    [basics] Test setup.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,6 +4,7 @@ import {Header} from '../header/header/header';
 export function App() {
   return (
     <main className='container main-wrapper'>
+      Dmitry Senior Full-Stack Developer
       <Header></Header>
       <div>content</div>
       <div>side</div>
