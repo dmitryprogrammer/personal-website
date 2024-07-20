358f590d51db920bca77b0dc37f20a836a8a8417
dmitryprogrammer <dpischalka@gmail.com>
2024-07-20 14:42:13 +0400

    Listed instead manual filling

--- src/components/header/nav-menu/nav-menu.tsx
+++ src/components/header/nav-menu/nav-menu.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 import './nav-menu.scss';
 
-const MENU_ITEMS = [
-"Main",
-"Blog",
-"Books I've Read",
-"Contacts",
-]
+const MENU_ITEMS = ['Main', 'Blog', "Books I've Read", 'Contacts'];
 
 export const NavMenu = ({hostClassName = ''}: {hostClassName?: string}) => {
   return (
     <nav className={'navigation-menu ' + hostClassName}>
       <ul className="navigation-menu__list">
-        {MENU_ITEMS.map((menuItem: string, i: number) => <li className="navigation-menu__item" key={i}>{menuItem}</li>)}
+        {MENU_ITEMS.map((menuItem: string, i: number) => (
+          <li className="navigation-menu__item" key={i}>
+            {menuItem}
+          </li>
+        ))}
       </ul>
     </nav>
   );
