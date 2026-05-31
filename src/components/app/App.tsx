98258aaa8262a229d0aec2e685ba47457267ac73
dmitryprogrammer <dpischalka@gmail.com>
2026-05-31 17:09:00 +0300

    added backround to the app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,18 @@
 import {Outlet} from "react-router";
 import {Header} from "../header/header/header";
 import "./App.scss";
+import {Background} from "../background/Background";
 
 export default function App() {
   return (
-    <main className="main-wrapper">
-      <Header />
-      <div className="main-container container">
-        <Outlet />
-      </div>
-    </main>
+    <>
+      <Background theme="light" />
+      <main className="main-wrapper">
+        <Header />
+        <div className="main-container container">
+          <Outlet />
+        </div>
+      </main>
+    </>
   );
 }
