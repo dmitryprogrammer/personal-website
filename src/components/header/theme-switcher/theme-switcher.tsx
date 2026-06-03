9df343c9f1e1a02f4a8c2aac1947143ef3e2bb74
dmitryprogrammer <dpischalka@gmail.com>
2026-06-03 23:41:49 +0300

    Base for theme switch

--- src/components/header/theme-switcher/theme-switcher.tsx
+++ src/components/header/theme-switcher/theme-switcher.tsx
@@ -1,15 +1,15 @@
-import {useState} from "react";
 import {THEMES} from "../../../config/themes";
+import {useTheme} from "../../../core/hooks/useTheme";
 import "./theme-switcher.scss";
 
 export const ThemeSwitcher = () => {
-  const [, setTheme] = useState(THEMES.LIGHT);
+  const {theme, setTheme} = useTheme();
 
   return (
     <ul className="theme-switcher-list">
       <li className="theme-switcher-list__item">
         <button
-          className="theme-switcher theme-switcher_active"
+          className={`theme-switcher ${theme === THEMES.LIGHT ? "theme-switcher_active" : ""}`}
           onClick={() => setTheme(THEMES.LIGHT)}
         >
           <span className="theme-switcher__icon">Light</span>
@@ -17,7 +17,7 @@ export const ThemeSwitcher = () => {
       </li>
       <li className="theme-switcher-list__item">
         <button
-          className="theme-switcher theme-switcher_active"
+          className={`theme-switcher ${theme === THEMES.DARK ? "theme-switcher_active" : ""}`}
           onClick={() => setTheme(THEMES.DARK)}
         >
           <span className="theme-switcher__icon">Dark</span>
