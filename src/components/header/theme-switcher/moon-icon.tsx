04893e628bba549b337572ca22d5ec26282c396e
dmitryprogrammer <dpischalka@gmail.com>
2026-06-04 23:53:03 +0300

    created themes icon

---
+++ src/components/header/theme-switcher/moon-icon.tsx
@@ -0,0 +1,17 @@
+import {SVGProps} from "react";
+
+export const MoonIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79Z"
+      fill="currentColor"
+    />
+  </svg>
+);
